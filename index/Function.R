fed76adbe888e2e7559f9355432947f508617cb5
NiloofarNL <91480518+NiloofarNL@users.noreply.github.com>
2023-10-19 00:40:11 -0400

    Comuna update

--- index/Function.R
+++ index/Function.R
@@ -3,7 +3,7 @@
 #' @return A data frame containing the loaded data.
 #' @export
 load_data_from_rda <- function() {
-  file_path2 <- system.file("data", "San_comuna.rda", package = "bSantiago")
-  San_comuna <- readRDS(file_path2)
-  return(San_comuna)
+  file_path2 <- system.file("data", "San_comuna2", package = "bSantiago")
+  San_comuna2 <- readRDS(file_path2)
+  return(San_comuna2)
 }
